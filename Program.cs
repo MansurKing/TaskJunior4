ba76d59dd6563658be7292a8cdc48e07b671aa51
MansurKing <102825225+MansurKing@users.noreply.github.com>
2024-08-13 20:39:05 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,14 +6,16 @@ namespace TaskJunior4
     {
         static void Main(string[] args)
         {
-            int picturesInTheAlbum = 52, groupOfImages = 3, displayingImagesInARow;
+            int picturesInTheAlbum = 52;
+            int groupOfImages = 3;
+            int displayingImagesInARow;
             float outputOfTheRemainingImages;
 
             displayingImagesInARow = picturesInTheAlbum / groupOfImages;
 
             outputOfTheRemainingImages = picturesInTheAlbum % groupOfImages;
 
-            Console.WriteLine($"Задача:\n\nВывод кол-ва картинок 3 в ряд:\n\n- полностью заполненых\n- остаток картинок\n\nДано:\n\n52 картинки\n\nРешение:\n");
+            Console.WriteLine("Задача:\n\nВывод кол-ва картинок 3 в ряд:\n\n- полностью заполненых\n- остаток картинок\n\nДано:\n\n52 картинки\n\nРешение:\n");
             Console.WriteLine($"- кол-во заполненых картинок 3 в ряд: {displayingImagesInARow}");
             Console.WriteLine($"- остаток картинок: {outputOfTheRemainingImages}");
         }
